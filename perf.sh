6a3772d518ee2c4ff362c6a657c37d0e109ac57b
jmbjr <boylejm@gmail.com>
2024-09-11 16:24:42 -0700

    Update perf.sh

--- perf.sh
+++ perf.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-echo "DANGEREROUS FORTRAN!"
-time ./dangerous_fortran
-echo "RESTRICTED C!"
-time ./restricted_c
-echo "safer FORTRAN!"
-time ./safer_fortran
-echo "unrestricted c!"
-time ./unrestricted_c
+# Function to run and time a program
+run_and_time() {
+    echo "$1"
+    time ./$2
+    sleep(1)
+}
+
+# Run and time each program
+run_and_time "DANGEROUS FORTRAN!" "dangerous_fortran"
+run_and_time "RESTRICTED C!" "restricted_c"
+run_and_time "SAFER FORTRAN!" "safer_fortran"
+run_and_time "UNRESTRICTED C!" "unrestricted_c"
+
 
 
